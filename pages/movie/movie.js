e9b5a208e301d6c9fc48e9c0ee3320748aa3b205
xiaofan <1353815633@qq.com>
2018-09-06 13:45:18 +0800

    上映页轮播图

--- pages/movie/movie.js
+++ pages/movie/movie.js
@@ -2,7 +2,28 @@
 Page({
 
   data: {
-    currentTab: 0
+    currentTab: 0,
+    scrWidth: 0,
+    scrHeight: 0,
+    imgUrls: [
+      '../images/haibao/1.jpg',
+      '../images/haibao/2.jpg',
+      '../images/haibao/3.jpg',
+      '../images/haibao/4.jpg'
+    ]
+  },
+
+  onLoad: function(e) {
+    var page = this;
+
+    wx.getSystemInfo({
+      success: function(res) {
+        page.setData({
+          scrWidth: res.screenWidth,
+          scrHeight: res.screenHeight
+        });
+      }
+    })
   },
 
   switchNav: function(e) {
